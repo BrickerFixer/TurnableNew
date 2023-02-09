9b18c921610b6f5190a60f1fa5765f67a64b09fe
BrickerFixer <107305222+BrickerFixer@users.noreply.github.com>
2023-02-09 12:48:43 +0400

    continue working on local file choosing

--- app/src/main/java/com/freeui/player/LocalActivity.java
+++ app/src/main/java/com/freeui/player/LocalActivity.java
@@ -2,13 +2,29 @@ package com.freeui.player;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.view.View;
+import android.widget.Button;
 
 public class LocalActivity extends AppCompatActivity {
-
+    Intent serviceIntent = new Intent(this, ExoplayerService.class);
+    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local);
+        Button addtr = (Button) findViewById(R.id.addfromlocbtt);
+        addtr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("audio/*");
+                startActivityForResult(intent, 2);
+            }
+        });
     }
 }
--- app/src/main/java/com/freeui/player/MainActivity.java
+++ app/src/main/java/com/freeui/player/MainActivity.java
@@ -225,15 +225,6 @@ public class MainActivity extends AppCompatActivity {
         bindService(serviceIntent, sConn, BIND_AUTO_CREATE);
         ExoPlayer player = ExoplayerService.player;
     }
-    private boolean isMyServiceRunning(Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
 class MyEventListener implements ExoPlayer.Listener {
     private TextView mTextView;
