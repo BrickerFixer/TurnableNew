d89073ecb1e22cd084af19919b2c5b86de99dbd6
brikerfixer <brickerfixer@gmail.com>
2023-02-14 18:20:49 +0400

    Add local playback! (Less goooo)

--- app/src/main/java/com/freeui/player/LocalActivity.java
+++ app/src/main/java/com/freeui/player/LocalActivity.java
@@ -1,5 +1,6 @@
 package com.freeui.player;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -11,8 +12,8 @@ import android.view.View;
 import android.widget.Button;
 
 public class LocalActivity extends AppCompatActivity {
-    Intent serviceIntent = new Intent(this, ExoplayerService.class);
-    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+    public static final int PICK_AUDIO_REQUEST = 1;
+    Uri uri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +22,26 @@ public class LocalActivity extends AppCompatActivity {
         addtr.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("audio/*");
-                startActivityForResult(intent, 2);
+                openFileChooser();
             }
         });
     }
+    public void openFileChooser(){
+        Intent intent = new Intent();
+        intent.setType("audio/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_AUDIO_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_AUDIO_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            Intent serviceIntent = new Intent(this, ExoplayerService.class);
+            uri = data.getData();
+            serviceIntent.putExtra("mediaitem", uri.toString());
+            startService(serviceIntent);
+        }
+    }
+
 }
